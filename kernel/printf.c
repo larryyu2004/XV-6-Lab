eff573bf01c358aa34dee85dc558a44b3e413c0c
larryyu2004 <jiayiyu@myyahoo.com>
2024-11-29 23:32:18 +1030

    Lab4

--- kernel/printf.c
+++ kernel/printf.c
@@ -139,7 +139,7 @@ backtrace()
   printf("backtrace:\n");
   uint fp = r_fp();
 
-  uint64 *frame = (uint64*) fp;
+  uint64 *frame = (uint64 *) fp;
   printf("frame[-1]: %p\n", frame[-1]);
   printf("frame[-2]: %p\n", frame[-2]);
   printf("%p\n", fp);
