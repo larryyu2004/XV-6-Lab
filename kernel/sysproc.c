12382762bf4365922f2592b45e0d67856fe82633
larryyu2004 <jiayiyu@myyahoo.com>
2024-12-01 22:55:29 +1030

    Lab4

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -120,5 +120,14 @@ sys_sigalarm(void)
 uint64 
 sys_sigreturn(void)
 {
-  return sigreturn();
+  struct proc *p = myproc();
+
+  if (!p->alarm_goingoff || !p->alarm_trapframe) {
+    return -1;
+  }
+
+  *p->trapframe = *p->alarm_trapframe;  // Restore original trapframe
+  p->alarm_goingoff = 0;
+
+  return 0;
 }
