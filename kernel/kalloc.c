602139837aac667ab82faa3928b4ca4c543e5960
larryyu2004 <jiayiyu@myyahoo.com>
2024-12-03 22:52:26 +1030

    lab5

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -9,8 +9,6 @@
 #include "riscv.h"
 #include "defs.h"
 
-
-
 void freerange(void *pa_start, void *pa_end);
 
 extern char end[]; // first address after kernel.
@@ -23,59 +21,14 @@ struct run {
 struct {
   struct spinlock lock;
   struct run *freelist;
-  // char* ref_page;
-  // int page_cnt;
-  // char* end_;
 } kmem;
-
-#define PA2PGREF_ID(p) (((p)-KERNBASE)/PGSIZE)
-#define PGREF_MAX_ENTRIES PA2PGREF_ID(PHYSTOP)
-struct spinlock pgreflock;
-int pageref[PGREF_MAX_ENTRIES];
-
-#define PA2PGREF(p) pageref[PA2PGREF_ID((uint64)(p))]
-
-
-// int pagecnt(void *pa_start, void *pa_end){
-//   char *p;
-//   int cnt = 0;
-//   p = (char*)PGROUNDUP((uint64)pa_start);
-//   for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
-//     cnt++;
-//   return cnt;
-// }
 
 void
 kinit()
 {
   initlock(&kmem.lock, "kmem");
-  initlock(&pgreflock, "pgref");
   freerange(end, (void*)PHYSTOP);
 }
-
-// int page_index(uint64 pa){
-//   pa = PGROUNDDOWN(pa);
-//   int res = (pa - (uint64) kmem.end_)/PGSIZE;
-//   if(res < 0 || res >= kmem.page_cnt){
-//     panic("page_index illegal");
-//   }
-//   return res;
-// }
-
-// void
-// incr(void* pa){
-//   int index = page_index((uint64)pa);
-//   acquire(&kmem.lock);
-//   kmem.ref_page[index]++;
-//   release(&kmem.lock);
-// }
-
-// void desc(void* pa){
-//   int index = page_index((uint64)pa);
-//   acquire(&kmem.lock);
-//   kmem.ref_page[index]--;
-//   release(&kmem.lock);
-// }
 
 void
 freerange(void *pa_start, void *pa_end)
@@ -93,30 +46,20 @@ freerange(void *pa_start, void *pa_end)
 void
 kfree(void *pa)
 {
-  // int index = page_index((uint64)pa);
-  // if(kmem.ref_page[index] > 1){
-  //   desc(pa);
-  //   return ;
-  // }
   struct run *r;
 
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
 
-  acquire(&pgreflock);
-  if(--PA2PGREF(pa) <= 0){
+  // Fill with junk to catch dangling refs.
+  memset(pa, 1, PGSIZE);
 
-    // Fill with junk to catch dangling refs.
-    memset(pa, 1, PGSIZE);
+  r = (struct run*)pa;
 
-    r = (struct run*)pa;
-
-    acquire(&kmem.lock);
-    r->next = kmem.freelist;
-    kmem.freelist = r;
-    release(&kmem.lock);
-  }
-  release(&pgreflock);
+  acquire(&kmem.lock);
+  r->next = kmem.freelist;
+  kmem.freelist = r;
+  release(&kmem.lock);
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -135,34 +78,5 @@ kalloc(void)
 
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
-    PA2PGREF(r) = 1;
-  //incr(r);
   return (void*)r;
-}
-
-void *kcopy_n_deref(void* pa){
-  acquire(&pgreflock);
-
-  if(PA2PGREF(pa) <= 1){
-    release(&pgreflock);
-    return pa;
-  }
-
-  uint64 newpa = (uint64)kalloc();
-  if(newpa == 0){
-    release(&pgreflock);
-    return 0;
-  }
-  memmove((void*)newpa, (void*)pa, PGSIZE);
-
-  PA2PGREF(pa)--;
-
-  release(&pgreflock);
-  return (void*)newpa;
-}
-
-void krefpage(void* pa){
-  acquire(&pgreflock);
-  PA2PGREF(pa)++;
-  release(&pgreflock);
 }
